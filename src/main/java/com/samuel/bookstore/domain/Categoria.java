4210e75222d77516316d49cff35923e6d64a1626
samuelalmeida95 <samuelalmeida95>
2021-04-04 12:01:00 -0300

    adicionando as anotações @Entity para a classe Categoria, e adicionando as anotações necessarias para o id da classe Categoria, fora isso também passei a anotação @OneToMany para a lista de Livros, assim o relacionamento entre as entidades fica resolvido

--- src/main/java/com/samuel/bookstore/domain/Categoria.java
+++ src/main/java/com/samuel/bookstore/domain/Categoria.java
@@ -1,13 +1,27 @@
 package com.samuel.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new LinkedList<>();
 
     public Categoria(Integer id, String nome, String descricao) {
