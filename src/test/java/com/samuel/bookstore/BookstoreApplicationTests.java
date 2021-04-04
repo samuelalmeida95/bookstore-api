d1502d77e15ef50af93a26644a6a5cbc42798726
samuelalmeida95 <samuelalmeida95>
2021-04-04 11:27:20 -0300

    primeiro commit

---
+++ src/test/java/com/samuel/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
