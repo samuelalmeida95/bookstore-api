34d3368f099e0e7afb2df2a66f4275d13e26a2ae
samuelalmeida95 <samuelalmeida95>
2021-04-04 12:03:07 -0300

    adicionando as anotações para a classe Livro, a principal mudança foi o relacionamento entre a entidade em questão (Livro), para a entidade Categoria, nesse caso é @ManyToOne, assim o relacionamento entre as entidades fica resolvido, um livro tem uma categoria, e uma categoria tem muitos livros

--- src/main/java/com/samuel/bookstore/domain/Livro.java
+++ src/main/java/com/samuel/bookstore/domain/Livro.java
@@ -1,14 +1,32 @@
 package com.samuel.bookstore.domain;
 
-public class Livro {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Livro implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro(int id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        
         this.id = id;
         this.titulo = titulo;
         this.nome_autor = nome_autor;
