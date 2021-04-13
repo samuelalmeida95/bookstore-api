f1d2c5256cdf50d13304bb6624503f4917172501
samuelalmeida95 <samuelalmeida95>
2021-04-13 11:09:00 -0300

    DELETE para categoria

--- src/main/java/com/samuel/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/samuel/bookstore/resource/CategoriaResource.java
@@ -10,6 +10,7 @@ import com.samuel.bookstore.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,12 @@ public class CategoriaResource {
     public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto) {
         Categoria newObj = service.update(id, objDto);
         return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/samuel/bookstore/services/CategoriaService.java
+++ src/main/java/com/samuel/bookstore/services/CategoriaService.java
@@ -39,5 +39,10 @@ public class CategoriaService {
         obj.setDescricao(objDto.getDescricao());
         return repository.save(obj);
     }
+    
+    public void delete(Integer id) {
+        findById(id);
+        repository.deleteById(id);
+    }
 
 }
