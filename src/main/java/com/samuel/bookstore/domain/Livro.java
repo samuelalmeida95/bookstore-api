9c35cbeb7c478de7e75f41c09510da1c0c2b33a3
samuelalmeida95 <samuelalmeida95>
2021-05-12 14:07:26 -0300

    validacao de campos para entidade Livro

--- src/main/java/com/samuel/bookstore/domain/Livro.java
+++ src/main/java/com/samuel/bookstore/domain/Livro.java
@@ -8,8 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Livro implements Serializable{
@@ -19,8 +22,16 @@ public class Livro implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotEmpty(message = "Campo TITULO é obrigatório!") 
+    @Length(min = 3, max = 100, message = "Campo TITULO deve ter entre 3 e 100 caracteres")
     private String titulo;
+
+    @NotEmpty(message = "Campo NOME AUTOR é obrigatório!") 
+    @Length(min = 3, max = 100, message = "Campo NOME AUTOR deve ter entre 3 e 100 caracteres")
     private String nomeAutor;
+
+
     private String texto;
 
     @JsonIgnore
