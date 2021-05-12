497864e15a96109ed665ec70ec25ac466aac9b2f
samuelalmeida95 <samuelalmeida95>
2021-05-12 12:16:45 -0300

    POST para Livros

--- src/main/java/com/samuel/bookstore/resource/LivroResource.java
+++ src/main/java/com/samuel/bookstore/resource/LivroResource.java
@@ -1,5 +1,6 @@
 package com.samuel.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -31,9 +35,24 @@ public class LivroResource {
     public ResponseEntity<List<LivroDTO>> findAll() {
         List<Livro> list = service.findAll();
         List<LivroDTO> listDTO = list.stream().map(livro -> new LivroDTO(livro)).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDTO);
-        
+        return ResponseEntity.ok().body(listDTO);    
     }
+
+    @PostMapping
+    public ResponseEntity<Livro> create(@RequestBody Livro livro) {
+        livro = service.create(livro);
+
+        URI uri = ServletUriComponentsBuilder
+                  .fromCurrentRequest()
+                  .path("{id}")
+                  .buildAndExpand(livro.getId())
+                  .toUri();
+
+        return ResponseEntity.created(uri).build();
+
+    }
+
+
     
 
 }
