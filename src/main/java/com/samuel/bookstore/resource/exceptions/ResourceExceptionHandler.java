3a5a5c61264c3b6cb463ad605ecfb72ea5305fe8
samuelalmeida95 <samuelalmeida95>
2021-05-12 14:26:18 -0300

    ajustes finais na API, agora caso o usuario tente cadastrar uma Categoria ou Livro sem os dados obrigatorios, a mensagem de erro ira ser retornada de forma personalizada, tonando a identificação do erro mais agradavel e compreensivel

--- src/main/java/com/samuel/bookstore/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/samuel/bookstore/resource/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,8 @@ import com.samuel.bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,6 +31,21 @@ public class ResourceExceptionHandler {
                 exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+
+        ValidationError error = new
+             ValidationError(System.currentTimeMillis()
+                        ,HttpStatus.BAD_REQUEST.value()
+                        ,"Erro na validação dos campos!");
+
+        for(FieldError erro: e.getBindingResult().getFieldErrors()) {
+            error.addErrors(erro.getField(),erro.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
    
