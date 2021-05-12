9c79d52779453d37fda9b813c84af42af5578a31
samuelalmeida95 <samuelalmeida95>
2021-05-12 13:12:27 -0300

    deixando o DTO de livros mais enxuto

--- src/main/java/com/samuel/bookstore/dtos/LivroDTO.java
+++ src/main/java/com/samuel/bookstore/dtos/LivroDTO.java
@@ -1,8 +1,12 @@
 package com.samuel.bookstore.dtos;
 
+import java.io.Serializable;
+
 import com.samuel.bookstore.domain.Livro;
 
-public class LivroDTO {
+public class LivroDTO  implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
 
     private String titulo;
     private String nomeAutor;
@@ -12,16 +16,21 @@ public class LivroDTO {
         this.nomeAutor = obj.getNomeAutor();
     }
 
+    public LivroDTO(){
+
+    }
+    
     public String getTitulo() {
         return titulo;
     }
-
+    
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
+    
     public String getNomeAutor() {
         return nomeAutor;
+
     }
 
     public void setNomeAutor(String nomeAutor) {
