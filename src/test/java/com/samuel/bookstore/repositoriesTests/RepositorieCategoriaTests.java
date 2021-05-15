db97fcdbd9578db5f60432b56e9ad41027c68053
samuelalmeida95 <samuelalmeida95>
2021-05-15 14:23:46 -0300

    refatorando chamada de uma nova categoria para que busque do pacote util a classe CategoriaCreator

--- src/test/java/com/samuel/bookstore/repositoriesTests/RepositorieCategoriaTests.java
+++ src/test/java/com/samuel/bookstore/repositoriesTests/RepositorieCategoriaTests.java
@@ -6,6 +6,7 @@ import javax.validation.ConstraintViolationException;
 
 import com.samuel.bookstore.model.Categoria;
 import com.samuel.bookstore.repositories.CategoriaRepository;
+import com.samuel.bookstore.util.CategoriaCreator;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -23,7 +24,7 @@ public class RepositorieCategoriaTests {
     @Test
     @DisplayName("Salvar e persistir categoria quando bem sucedido")
     void salvar_PersistirCategoriaQuandoBemSucedido(){
-        Categoria categoriaParaSerSalva = criarCategoria();
+        Categoria categoriaParaSerSalva = CategoriaCreator.criarCategoria();
         Categoria categoriaSalva = this.categoriaRepository.save(categoriaParaSerSalva);
 
         Assertions.assertThat(categoriaSalva).isNotNull();
@@ -46,7 +47,7 @@ public class RepositorieCategoriaTests {
     @Test
     @DisplayName("Salvar e atualizar categoria quando bem sucedido")
     void salvar_AtualizarCategoriaQuandoBemSucedido(){
-        Categoria categoriaParaSerSalva = criarCategoria();
+        Categoria categoriaParaSerSalva = CategoriaCreator.criarCategoria();
 
         Categoria categoriaSalva = this.categoriaRepository.save(categoriaParaSerSalva);
 
@@ -63,7 +64,7 @@ public class RepositorieCategoriaTests {
     @Test
     @DisplayName("Deletar e remover categoria quando bem sucedido")
     void deletar_RemoverCategoriaQuandoBemSucedido(){
-        Categoria categoriaParaSerSalva = criarCategoria();
+        Categoria categoriaParaSerSalva = CategoriaCreator.criarCategoria();
 
         Categoria categoriaSalva = this.categoriaRepository.save(categoriaParaSerSalva);
 
@@ -78,7 +79,7 @@ public class RepositorieCategoriaTests {
     @Test
     @DisplayName("Encontrar categoria por id quando bem sucedido")
     void encontrar_PorIdCategoriaQuandoBemSucedido(){
-        Categoria categoriaParaSerSalva = criarCategoria();
+        Categoria categoriaParaSerSalva = CategoriaCreator.criarCategoria();
 
         Categoria categoriaSalva = this.categoriaRepository.save(categoriaParaSerSalva);
 
@@ -97,12 +98,6 @@ public class RepositorieCategoriaTests {
         Optional<Categoria> categoriaOptional = this.categoriaRepository.findById(-1);
 
         Assertions.assertThat(categoriaOptional).isEmpty();
-    }
-
-
-    private Categoria criarCategoria(){
-        Categoria cat = new Categoria(null, "Testes", "Aprendendo sobre testes");
-        return cat;
     }
 
 }
