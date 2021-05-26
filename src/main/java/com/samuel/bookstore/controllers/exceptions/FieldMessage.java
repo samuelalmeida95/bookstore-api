bcbf730c4cebda8e6ab44af3c1fe0fb2c96fb2d8
samuelalmeida95 <samuelalmeida95>
2021-05-26 14:18:16 -0300

    removendo lombok da classe de erro fielmessage

--- src/main/java/com/samuel/bookstore/controllers/exceptions/FieldMessage.java
+++ src/main/java/com/samuel/bookstore/controllers/exceptions/FieldMessage.java
@@ -1,14 +1,31 @@
 package com.samuel.bookstore.controllers.exceptions;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class FieldMessage {
-    
+
     private String fieldName;
     private String message;
+
+    public FieldMessage() {
+    }
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
