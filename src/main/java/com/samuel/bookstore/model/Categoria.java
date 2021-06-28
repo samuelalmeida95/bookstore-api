4ce5f1c851592420ec6c836bd16db3edce1f7af5
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-06-27 23:11:56 -0300

    ajustanto classes sem o uso do lombok

--- src/main/java/com/samuel/bookstore/model/Categoria.java
+++ src/main/java/com/samuel/bookstore/model/Categoria.java
@@ -13,17 +13,14 @@ import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Categoria implements Serializable{
-    
+public class Categoria implements Serializable {
+
+    public Categoria(Object object, String string, String string2) {
+    }
+
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -31,14 +28,83 @@ public class Categoria implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotEmpty(message = "Campo NOME é obrigatório!") 
+    @NotEmpty(message = "Campo NOME é obrigatório!")
     @Length(min = 3, max = 100, message = "Campo NOME deve ter entre 3 e 100 caracteres")
     private String nome;
 
-    @NotEmpty(message = "Campo NOME é obrigatório!") 
+    @NotEmpty(message = "Campo NOME é obrigatório!")
     @Length(min = 3, max = 100, message = "Campo NOME deve ter entre 3 e 100 caracteres")
     private String descricao;
 
     @OneToMany(mappedBy = "categoria") // um para muitos
     private List<Livro> livros = new LinkedList<>();
+
+    public Categoria(Integer id,
+            @NotEmpty(message = "Campo NOME é obrigatório!") @Length(min = 3, max = 100, message = "Campo NOME deve ter entre 3 e 100 caracteres") String nome,
+            @NotEmpty(message = "Campo NOME é obrigatório!") @Length(min = 3, max = 100, message = "Campo NOME deve ter entre 3 e 100 caracteres") String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Categoria() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categoria other = (Categoria) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
--- src/main/java/com/samuel/bookstore/model/Livro.java
+++ src/main/java/com/samuel/bookstore/model/Livro.java
@@ -1,7 +1,5 @@
 package com.samuel.bookstore.model;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,40 +9,110 @@ import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import com.samuel.bookstore.audits.TimestampAudit;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import org.hibernate.validator.constraints.Length;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Livro extends TimestampAudit {
-    
+
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotEmpty(message = "Campo TITULO é obrigatório!") 
-    @Length(min = 3, max = 100, message = "Campo TITULO deve ter entre 3 e 100 caracteres")
     private String titulo;
 
-    @NotEmpty(message = "Campo NOME AUTOR é obrigatório!") 
-    @Length(min = 3, max = 100, message = "Campo NOME AUTOR deve ter entre 3 e 100 caracteres")
     private String nomeAutor;
-
 
     private String texto;
 
     @JsonIgnore
-    @ManyToOne //muitos para um
-    @JoinColumn(name = "categoria_id") //coluna de juncao
+    @ManyToOne // muitos para um
+    @JoinColumn(name = "categoria_id") // coluna de juncao
     private Categoria categoria;
 
+    public Livro(Integer id,
+            @NotEmpty(message = "Campo TITULO é obrigatório!") @Length(min = 3, max = 100, message = "Campo TITULO deve ter entre 3 e 100 caracteres") String titulo,
+            @NotEmpty(message = "Campo NOME AUTOR é obrigatório!") @Length(min = 3, max = 100, message = "Campo NOME AUTOR deve ter entre 3 e 100 caracteres") String nomeAutor,
+            String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
 
+    public Livro() {
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
